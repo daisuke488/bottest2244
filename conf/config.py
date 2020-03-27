96c72a3247b2cd67fe248cecdabe34759cea3da4
daisuke <daisuke904@gmail.com>
2020-03-28 07:05:10 +0900

    add new file

---
+++ conf/config.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2020-present Works Mobile Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+# default language ['en'|'ko'|'ja']
+DEFAULT_LANG = 'ja'
+
+# TimeZone
+TZone = "Asia/Tokyo"
+
+# account
+ADMIN_ACCOUNT = "taro987@works-990435"
+DOMAIN_ID = 10164628
+
+# api
+API_ID = "jp1fexMGJawef"
+SERVER_CONSUMER_KEY = "9BdB0LPs8jZNXKxvafYz"
+
+SERVER_ID = "bffbaf07c0df4948a50a90d12db91958"
+SECRET_KEY_NAME = "private_20200326103503.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://bottest2288.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-3-223-21-106.compute-1.amazonaws.com"
+DB_NAME = "d706f0m04k93j3"
+DB_USER = "pezaqxeukybhyw"
+DB_PORT = "5432"
+DB_PASSWORD = "511ff2b9fccec18ee8bce2aeb20fd844fc0be79aef584c75378e4163aa099b7b"
